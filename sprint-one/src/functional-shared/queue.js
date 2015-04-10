b26a5a8c2a1f62ba9ec1589f6d0efc23b7cbda91
Christian <crisxian@users.noreply.github.com>
2015-04-09 22:50:15 -0700

    data struct 1: functional with shared methods
    - refactor queue and stack to have share methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,9 +3,34 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.enter = 0;
+  instance.exit = 0;
+
+  instance.enqueue = queueMethods.enqueue;
+
+  instance.dequeue = queueMethods.dequeue;
+
+  instance.size = queueMethods.size;
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.storage[this.enter++] = value;
+};
 
+queueMethods.dequeue = function() {
+	if(this.enter === this.exit) return; // empty?
 
+	var val = this.storage[this.exit]
+	delete this.storage[this.exit++];
+	return val;
+};
+
+queueMethods.size = function() {
+	return this.enter - this.exit;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,8 +3,34 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.height = 0;
+
+  instance.push = stackMethods.push;
+
+  instance.pop = stackMethods.pop;
+
+  instance.size = stackMethods.size;
+
+  return instance;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+	this.storage[this.height++] = value;
+};
 
+stackMethods.pop = function() {
+	if(!this.height) return; // empty?
+
+	var val = this.storage[--this.height];
+	delete this.storage[this.height];
+	return val;
+};
+
+stackMethods.size = function() {
+	return this.height;
+};
