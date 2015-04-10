96a28cfb758053a94b11a744725b7e93c9b61c87
Christian <crisxian@users.noreply.github.com>
2015-04-09 22:02:42 -0700

    data struct 1: functional style
    - stack push / pop
    - queue / dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,31 @@
 // problem set written by Hack Reactor. challenge files cloned on 4/9/2015 for educational purposes.
 // CHALLENGES AND FUNCTIONS COMPLETED BY: github.com/crisxian
+
+// first in, first out
 var makeQueue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // indices
+  var enter = 0; // enqueue increments
+  var exit = 0; // dequeue increments
 
   // Implement the methods below
   someInstance.enqueue = function(value){
+    storage[enter++] = value;
   };
 
   someInstance.dequeue = function(){
+    if(exit === enter) return;
+
+    var value = storage[exit];
+    delete storage[exit++]; // delete, increment exit index, take val
+    return value;
   };
 
   someInstance.size = function(){
+    return enter - exit;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,19 +1,29 @@
 // problem set written by Hack Reactor. challenge files cloned on 4/9/2015 for educational purposes.
 // CHALLENGES AND FUNCTIONS COMPLETED BY: github.com/crisxian
+
+// last in, first out
 var makeStack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var height = 0; // inc with push, decr with pop
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[height++] = value;
   };
 
   someInstance.pop = function(){
+    if(!height) return;
+
+    var value = storage[--height]
+    delete storage[height];
+    return value;
   };
 
   someInstance.size = function(){
+    return height;
   };
 
   return someInstance;
