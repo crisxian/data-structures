4fe74d848be747395df015f23dd1b190ad8e9b3e
Christian <crisxian@users.noreply.github.com>
2015-04-09 23:17:48 -0700

    data struct 1: prototypal method inheritance
    - refactor stack and queue to inherit methods of another object

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,8 +3,26 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.enter = instance.exit = 0;
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.storage[this.enter++] = value;
+};
 
+queueMethods.dequeue = function() {
+	if(this.enter === this.exit) return; // empty?
+
+	var value  = this.storage[this.exit]
+	delete this.storage[this.exit++]
+	return value;
+};
+
+queueMethods.size = function() {
+	return this.enter - this.exit;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,8 +3,26 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.height = 0;
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+	this.storage[this.height++] = value; 
+};
 
+stackMethods.pop = function() {
+	if(!this.height) return; // empty?
+
+	var value = this.storage[--this.height];
+	delete this.storage[this.height]
+	return value;
+};
+
+stackMethods.size = function() {
+	return this.height;
+};
