5de04cc617fbdef9ff53ee6017977ebd236205f2
Christian <crisxian@users.noreply.github.com>
2015-04-09 23:48:04 -0700

    data struct 1: pseudoclassical prototypal instantiation
    - common instantiation pattern for prototype inheritance
    - refactor stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,6 +3,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.enter = this.exit = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+	this.storage[this.enter++] = value; // enter val then increment to next empty
+};
+
+Queue.prototype.dequeue = function() {
+	if(this.enter === this.exit) return; // empty?
+
+	var value = this.storage[this.exit];
+	delete this.storage[this.exit++]; // remove data then increment next value
+	return value;
+};
+
+Queue.prototype.size = function() {
+	return this.enter - this.exit;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,6 +3,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.height = 0;
 };
 
+Stack.prototype.push = function(value) {
+	this.storage[this.height++] = value;
+};
 
+Stack.prototype.pop = function() {
+	this.height && this.height--; // short-circuit
+
+	var value = this.storage[this.height];
+	delete this.storage[this.height];
+	return value;
+};
+
+Stack.prototype.size = function() {
+	return this.height;
+};
