348023d9b19c28e5268f35554aa8b9467e48d9fe
Christian <crisxian@users.noreply.github.com>
2015-04-10 22:41:45 -0700

    data struct 2: tree implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,34 @@
 // problem set written by Hack Reactor. challenge files cloned on 4/9/2015 for educational purposes.
 // CHALLENGES AND FUNCTIONS COMPLETED BY: github.com/crisxian
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+	if(this.value === target) return true;
 
+	if(this.children) {
+		for (var node in this.children) {
+			if(this.children[node].contains(target)) {
+				return true;
+			}
+		};
+	};
+
+	return false;
 };
 
 
